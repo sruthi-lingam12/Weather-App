f5c04a267865216398a20f10804843d4460fc9c4
Sruthi Lingam <143719881+sruthi-lingam12@users.noreply.github.com>
2024-07-29 16:28:42 -0400

    first commit

---
+++ Weather/Weather/Views/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Sruthi Lingam on 7/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
+    var body: some View {
+        VStack {
+            if let location = locationManager.location {
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView().environmentObject     (locationManager)
+                }
+            }
+            
+            
+            
+        }
+        .background(Color(hue: 0.573, saturation: 0.446, brightness: 0.853))
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
