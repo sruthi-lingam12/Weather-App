6acb28c63fc8bdd5e7004ee0cdd2befe5008c181
Sruthi Lingam <143719881+sruthi-lingam12@users.noreply.github.com>
2024-07-29 16:30:49 -0400

    Add files via upload

---
+++ Weather/Weather/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  Weather
+//
+//  Created by Sruthi Lingam on 7/29/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
